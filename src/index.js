45ba4c0a5f676446556f2cb400889c02216956d5
Diana Nyokabi <nyokabidiana98@gmail.com>
2024-08-22 21:27:24 +0300

    injected more weather data

--- src/index.js
+++ src/index.js
@@ -2,8 +2,38 @@ function refreshWeather(response) {
     let temperatureElemet = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let humidityElement = document.querySelector("#humidity");
+    let timeElement = document.querySelector("#time");
+    let date = new Date (response.data.time * 1000);
+    
     cityElement.innerHTML = response.data.city;
+    timeElement.innerHTML = formatDate(date);
+    descriptionElement.innerHTML = response.data.condition.description;
+    windSpeedElement.innerHTML = `${response.data.wind.speed}Km/h`;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
     temperatureElemet.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date) {
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Sartuday",
+
+    ];
+    let day = days[date.getDay()];
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    return `${day} ${hours}:${minutes}`;
 }
 function searchCity(city) {
     let apiKey = "157a76fde4bot20af4447b7a34072da8";
